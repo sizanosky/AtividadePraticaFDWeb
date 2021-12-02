0e67dc55061f1d057f5c18c2ba46517c3e04487c
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-12-02 01:49:52 -0300

    update

---
+++ src/app/app.alunos.ts
@@ -0,0 +1,33 @@
+export const alunos = [
+    {
+        nome: "Marcos Fabricio Sizanosky",
+        ru: "3395570",
+        curso: "Análise e Desenvolvimento de Sistemas",
+        dt_nascimento: "04/11/1985"
+    },
+
+    {
+        nome: "Priscila Bruske da Silva",
+        ru: "9985712",
+        curso: "Administração",
+        dt_nascimento: "17/06/1982"
+    },
+    {
+        nome: "Maria Clara Bruske Sizanosky",
+        ru: "9964352",
+        curso: "Arquitetura",
+        dt_nascimento: "15/08/2007"
+    },
+    {
+        nome: "Lucas Bruske Sizanosky",
+        ru: "3453366",
+        curso: "Engenharia de Software",
+        dt_nascimento: "12/01/2012"
+    },
+    {
+        nome: "João Luiz da Cunha",
+        ru: "3673779",
+        curso: "Gestão Publica",
+        dt_nascimento: "14/01/1964"
+    }
+]
